e27ee4a174b704bcc0bab7a0d8714634fe4fd521
Виктория Сопина <vt.sopina@yandex.ru>
2023-04-15 01:25:20 +0300

    add 3 funk

--- homework.py
+++ homework.py
@@ -1,12 +1,16 @@
+import json
 import os
 from time import time
 import requests
 import requests.exceptions as ex
 
 from dotenv import load_dotenv
-from typing import Final, Optional
+import jsonschema
+from jsonschema.exceptions import ValidationError
 import logging
-from telegram import Bot
+from telegram import Bot, error
+from typing import Final, Optional
+
 
 load_dotenv()
 
@@ -25,10 +29,15 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-START, FAIL, OK = ' start', ' FAIL', ' done'
+START, FAIL, OK = ' start', ' ...FAIL', ' ...done'
+UNKNOWN_ERROR = '...UNKNOWN_ERROR'
+PROGRAM_STOPPED = 'Программа принудительно остановлена'
 CHECK_ENV = 'Пероверка переменных окружения,'
 GET_HOMEWORKS = 'Получение пакета с домашними работами,'
 POST_CHAT = 'Отправка сообщения в чат Telegram,'
+CHECK_API_RESPONSE = 'Проверка ответа API на сооветствие документации,'
+GET_STATUS_HOMEWORK = 'Получение статуса домашней работы,'
+HOMEWORK_STATUS_CHANGE = 'Изменился статус проверки работы "{0}". {1}'
 
 
 def check_tokens() -> Optional[bool]:
@@ -38,19 +47,20 @@ def check_tokens() -> Optional[bool]:
     for token in tokens:  # name_token, token in tokens.items()
         if token:
             continue
-        logging.critical(CHECK_ENV, FAIL)
+        logging.critical(CHECK_ENV, FAIL, PROGRAM_STOPPED)
         raise EnvironmentError
-    logging.info(CHECK_ENV, OK)
+    logging.debug(CHECK_ENV, OK)
     return True
 
 
 def send_message(bot: Bot, message: str) -> None:
     """Отправка сообщения в чат Telegram."""
+    logging.info(POST_CHAT, START)
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logging.debug(POST_CHAT, OK)
-    except:
-        logging.error(POST_CHAT, FAIL)
+        logging.debug(POST_CHAT, OK)    # прописать пользовательский класс
+    except error.TelegramError as err:
+        logging.error(POST_CHAT, FAIL, err)
 
 
 def get_api_answer(timestamp: int) -> Optional[dict]:
@@ -62,29 +72,52 @@ def get_api_answer(timestamp: int) -> Optional[dict]:
                                 params={'from_date': timestamp},
                                 timeout=10)
         response.raise_for_status()
-        logging.info(GET_HOMEWORKS, OK)
+        logging.debug(GET_HOMEWORKS, OK)
         return response.json()
-    except ex.HTTPError as err:    # Попробовать потом в скобки бахнуть все ошибки разом?
+    except ex.HTTPError(response) as err:
         logging.error(GET_HOMEWORKS, FAIL, err)
-    except (ex.ConnectionError, ex.Timeout) as err:  # Сетевые проблемы
+    except (ex.ConnectionError, ex.Timeout) as err:
         logging.error(GET_HOMEWORKS, FAIL, err)
-    except ex.RequestException as err:               #  Все остальное
-        logging.error(GET_HOMEWORKS, FAIL, err)
+    except ex.RequestException as err:
+        logging.error(GET_HOMEWORKS, UNKNOWN_ERROR, err)
 
 
-def check_response(response):
+def check_response(response) -> Optional[bool]:
     """Проверяет ответ API на соответствие документации."""
-    ...
+    logging.info(CHECK_API_RESPONSE, START)
+    try:
+        with open('schema.json') as f:
+            SCHEMA = json.load(f)
+        jsonschema.validate(response, SCHEMA)
+        logging.debug(CHECK_API_RESPONSE, OK)
+        return True
+    except ValidationError as err:
+        logging.error(CHECK_API_RESPONSE, FAIL, err)
+    except Exception as err:
+        logging.error(CHECK_API_RESPONSE, UNKNOWN_ERROR, err)
 
 
-def parse_status(homework):
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+def parse_status(homework) -> str:
+    """
+    Извлекает из информации о конкретной домашней
+    работе статус этой работы.
+    """
+    logging.info(GET_STATUS_HOMEWORK, START)
+    try:
+        if isinstance(homework, dict):
+            homework_name = homework['homework_name']
+            verdict = HOMEWORK_VERDICTS[homework['status']]
+            return HOMEWORK_STATUS_CHANGE.format(homework_name, verdict)
+        else:
+            raise TypeError
+    except (KeyError, TypeError) as err:
+        logging.error(GET_STATUS_HOMEWORK, FAIL, err)
 
 
 def main():
     """Основная логика работы бота."""
+    check_tokens()
+
 
     ...
 
@@ -101,7 +134,6 @@ def main():
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             ...
-        ...
 
 
 if __name__ == '__main__':
